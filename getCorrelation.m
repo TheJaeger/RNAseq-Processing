7976d0cbe6b0df9ecd1680cab1f58876dc8c47ce
Siddhartha Dhiman <dhiman@musc.edu>
2019-05-15 22:31:39 -0400

    Add correlation computation with sparse conversion

--- getCorrelation.m
+++ getCorrelation.m
@@ -41,11 +41,11 @@ end
 
 %% Load Data File
 [num,txt,raw] = xlsread(xlsPath);
-[nN nR] = size(num);
+[~,fn,~] = fileparts(xlsPath);
 
 %  Perform check on number of triplicates specified. Try to use number of
-%  columns divided by two. If that fails, default to triplicates where R =
-%  3
+%  columns divided by two. If fails, default to triplicates where R = 3.
+[nN nR] = size(num);
 if exist('R','var') || ~isempty(R)
     if rem(nR,R) == 0
         disp(sprintf('Specified R = %d matches Excel',R));
@@ -78,13 +78,19 @@ num = num(idxNaN,:);
 %% Standardize Data (Entire Row)
 num = zscore(num,0,2);
 
-%% Independent Conditions
-Cond1 = numStand(:,1:3);
-Cond2 = numStand(:,4:6);
+%% Form Independent Conditions
+%  R1 --> First condition
+%  R2 --> Second condition
+%  S --> Starting columnwise index of replicate
+%  E --> Ending columnwise index of replicate
+R1_S = 1;
+R1_E = R;
+R2_S = R + 1;
+R2_E = R * 2;
 
 %% Find Correlation
-corrMat1 = corr(transpose(Cond1));
-corrMat2 = corr(transpose(Cond2));
+corrMat1 = corr(transpose(num(:,R1_S:R1_E)));
+corrMat2 = corr(transpose(num(:,R2_S:R2_E)));
 
 %   Obtain Upper Triangle
 corrMat1 = triu(corrMat1,1);
@@ -94,57 +100,9 @@ corrMat2 = triu(corrMat2,1);
 corrMat1 = sparse(corrMat1);
 corrMat2 = sparse(corrMat2);
 
-%% Form Matrix for CytoScape
-%   For C
-[G1 G2 corr1] = find(corrMat1);
-G1 = cell2table(geneList(G1));
-G1.Properties.VariableNames{'Var1'} = 'G1';
-G2 = cell2table(geneList(G2));
-G2.Properties.VariableNames{'Var1'} = 'G2';
-corr1 = array2table(corr1);
-corr1.Properties.VariableNames{'corr1'} = 'Corr';
-data1 = [G1 G2 corr1];
-data1 = rmmissing(data1);
-data1 = sortrows(data1,3,'descend');
 
-%   For CN
-[G1 G2 corr2] = find(corrMat2);
-G1 = cell2table(geneList(G1));
-G1.Properties.VariableNames{'Var1'} = 'G1';
-G2 = cell2table(geneList(G2));
-G2.Properties.VariableNames{'Var1'} = 'G2';
-corr2 = array2table(corr2);
-corr2.Properties.VariableNames{'corr2'} = 'Corr';
-data2 = [G1 G2 corr2];
-data2 = rmmissing(data2);
-data2 = sortrows(data2,3,'descend');
 
-bins = 100;
-gca = figure;
-hold on;
-histogram(table2array(corr1),bins,...
-    'EdgeAlpha',0,...
-    'FaceColor','r',...
-    'FaceAlpha',0.5);
-histogram(table2array(corr2),bins,...
-    'EdgeAlpha',0,...
-    'FaceColor','b',...
-    'FaceAlpha',0.5);
-hold off;
-xlabel('Correlation');
-ylabel('Frequency');
-legend('C','CTK','Location','northwest');
-grid on; box on; axis tight;
-title('Absolute Pairwise Gene Correlations in N and NTK');
-print('histogram','-djpeg','-r600');
 
-% Take top 20,000 Correlations in each condition
-data1 = data1(1:20000,:);
-data2 = data2(1:20000,:);
-
-% Save Tables as CSV
-writetable(data1,'corrN.xlsx');
-writetable(data2,'corrNTK.xlsx');
 
 
 end
