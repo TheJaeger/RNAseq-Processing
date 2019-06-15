6ced253e2eb27a998d5cf60253f04fe79c4f3c54
Siddhartha Dhiman <dhiman@musc.edu>
2019-06-15 09:27:55 -0400

    Backbone foldchange()

--- foldchange.m
+++ foldchange.m
@@ -29,3 +29,98 @@ function changeMat = foldchange(xlsPath,varargin)
 % Author: Siddhartha Dhiman
 % Email: sdhiman@buffalo.edu & dhiman@musc.edu
 % Created with MATLAB 2019a
+
+%% Parse Inputs
+tic;
+defaultR = 3;
+defaultOut = pwd;
+
+p = inputParser;
+p.addRequired('xlsPath',@isstr);
+p.addOptional('R',defaultR,@(x) rem(x,1)==0);
+p.addOptional('outPath',defaultOut,@isstr);
+
+parse(p,xlsPath,varargin{:});
+
+R = p.Results.R;
+outPath = p.Results.outPath;
+
+%% Perform Checks
+%  Close any open parallel loops
+closeParallel;
+
+%  Check for file existence
+if ~exist(xlsPath); error('Input file does not exist'); end
+
+%  Check whether file can be opened
+try
+    tmp = xlsfinfo(xlsPath);
+    disp('Input is a valid Excel file');
+catch
+    error('Excel file cannot be read. Ensure compatibility with your MATLAB version');
+end
+
+%  Check whether output exists. If not, save current working directory
+if ~exist('outPath','var') || isempty(outPath)
+    outPath = pwd;
+end
+
+%% Load Data File
+
+[num,txt,raw] = xlsread(xlsPath);
+[~,fn,~] = fileparts(xlsPath);
+savePath = fullfile(outPath,fn);
+mkdir(savePath);
+
+%  Perform check on number of triplicates specified. Try to use number of
+%  columns divided by two. If fails, default to triplicates where R = 3.
+[nN nR] = size(num);
+if exist('R','var') || ~isempty(R)
+    if rem(nR,R) == 0
+        disp(sprintf('Specified R = %d matches dataset',R));
+    else
+        tmp = nR/2;
+        if isreal(tmp) && rem(tmp,1)==0
+            warning(sprintf('Specified R = %d does not match dataset. Determined R = %d.\n',R,tmp));
+            R = tmp;
+        else
+            R = 3;
+            warning(sprintf('Specified R = %d does not match dataset. Using default R = 3.\n',R));
+        end
+    end
+end
+
+geneList = txt(1:end,1);
+
+%  Check for NaN and Remove them
+idxNaN = ~any(isnan(num),2);
+if numel(find(idxNaN == 0)) > 0
+    disp(sprintf('Found %d genes with missing observations or replicates < R...discarding\n',numel(find(idxNaN == 0))));
+else
+    fprintf('All genes have replicates = R...data is excellent.\n');
+end
+geneList = geneList(idxNaN);
+num = num(idxNaN,:);
+
+
+
+
+
+%% Dependent Functions
+% Open Parallel Pool
+    function openParallel
+        %  Queries the maximum number of logical cores avaialble and opens
+        %  them for computation
+        numworkers = feature('numcores');
+        disp(sprintf('Parallel Processing: Found %d logical cores',numworkers));
+        parObj = parpool('local',numworkers);
+    end  % openParallel end
+
+%  Close Parallel Pool
+    function closeParallel
+        %  Closes any open parallel pools
+        parObj = gcp('nocreate');
+        delete(parObj);
+    end  % closeParallel end
+toc;
+end  % getCorrelation end
