9442eb3c916941d2c6d8fcb854d2d3667949b949
Siddhartha Dhiman <dhiman@musc.edu>
2020-01-25 12:36:35 -0500

    Update processing script

--- Processing_Script.m
+++ Processing_Script.m
@@ -1,7 +1,7 @@
 clc; clear all; close all;
 
 %% Define Paths for Input and Output
-inPath = 'D:\Datasets\RNAS-Seq\mRNA\Triplicates';
+inPath = 'D:\Datasets\RNAS-Seq\mRNA\Triplicates_Cleaned';
 targetPath = 'D:\Datasets\RNAS-Seq\mRNA\Targets';
 outPath = 'D:\SystemFiles\siddh\Downloads\RNA-Seq';
 R = 3;
@@ -13,7 +13,7 @@ dirTarget = dir(fullfile(targetPath,'*xlsx'));
 for i = 1:length(dirIn)
     path2File = fullfile(dirIn(i).folder,dirIn(i).name);
     getCorrelation(path2File,R,outPath);
-    foldchange(path2File,R,outPath);
+    %foldchange(path2File,R,outPath);
 end
 
 for i = 1:length(dirTarget)
